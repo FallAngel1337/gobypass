04231994f4bc67457381c79edeab7532bd51208a
FallAngel1337 <62574781+FallAngel1337@users.noreply.github.com>
2020-10-15 17:16:26 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"os"
+	"sync"
+
+	"github.com/gobypass"
+)
+
+var (
+	urls    string
+	threads int
+	wg      sync.WaitGroup
+)
+
+func main() {
+	flag.StringVar(&urls, "u", "", "(Optional) The list containing the 403 urls")
+	flag.IntVar(&threads, "t", 1, "The number of threads")
+	flag.Parse()
+
+	wg.Add(threads)
+
+	if urls != "" {
+		fn, err := os.Open(urls)
+		gobypass.CheckError(err)
+
+		reader := bufio.NewScanner(fn)
+		urls := make([]string, 0)
+
+		for reader.Scan() {
+			urls = append(urls, reader.Text())
+		}
+
+		for i := 0; i < threads; i++ {
+			go gobypass.Bypass(&urls, &wg)
+		}
+	} else {
+		reader := bufio.NewScanner(os.Stdin)
+		urls := make([]string, 0)
+
+		for reader.Scan() {
+			urls = append(urls, reader.Text())
+		}
+
+		for i := 0; i < threads; i++ {
+			go gobypass.Bypass(&urls, &wg)
+		}
+	}
+
+	wg.Wait()
+}
